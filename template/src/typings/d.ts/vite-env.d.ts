6ce0ac6b54f85b57e388bd5857ab00dd73a413c8
eirc.yuan <twotwoba@126.com>
2025-03-11 16:36:47 +0800

    update template env declare

--- template/src/typings/d.ts/vite-env.d.ts
+++ template/src/typings/d.ts/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    /** app title */
+    VITE_TITLE: string
+    /** router mode */
+    VITE_USE_HASH: string
+    /** axios baseURL */
+    VITE_AXIOS_BASE_URL: string
+    /** home path */
+    VITE_HOME_PATH: string
+    /** public path */
+    VITE_PUBLIC_PATH: string
+}
