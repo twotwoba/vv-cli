cf809ae47151d4c06989cebd7e339de572f8da01
Eric <twotwoba@126.com>
2025-09-08 15:51:53 +0800

    update template-vue  dependenices

--- template-vue/src/views/hello-world/index.vue
+++ template-vue/src/views/hello-world/index.vue
@@ -4,14 +4,8 @@ import { useThemeStore } from '@/store/modules/theme-store'
 const { toggle } = useThemeStore()
 </script>
 <template>
-    <div class="flex-cc gap-12 pt-20%" text-12>
-        <span>Hello World</span>
-        <svg-icon name="vue"></svg-icon>
-        <div class="i-logos:vitejs"></div>
-        <div
-            class="dark:i-line-md:sunny-filled-loop-to-moon-filled-loop-transition i-line-md:moon-filled-to-sunny-filled-loop-transition cursor-pointer text-#fcc21b dark:text-#ffe200"
-            @click="toggle()"
-        ></div>
-        <n-button type="info">Info</n-button>
-    </div>
+<div class="flex-cc gap-12 pt-20%" text-12>
+    <svg-icon name="vue"></svg-icon>
+    <n-button type="info" @click="() => toggle()">CHANGE THEME</n-button>
+</div>
 </template>
