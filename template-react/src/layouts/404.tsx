d1aead53f86c6197fa2c5773b32cf9fec17af66d
Eric <ericyuanovo@gmail.com>
2025-12-05 15:33:37 +0800

    fix: spell error

--- template-react/src/layouts/404.tsx
+++ template-react/src/layouts/404.tsx
@@ -1,5 +1,5 @@
 import { ReactSVG } from "react-svg"
-import UnexpectedError from "@/components/upexpected-error"
+import UnexpectedError from "@/components/unexpected-error"
 
 const NotFound = () => (
 	<UnexpectedError icon={<ReactSVG src="src/assets/icons/404.svg" />} errorMsg="Not Found" />
