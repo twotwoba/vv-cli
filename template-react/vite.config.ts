0680f0d816bf966ffd19a4a61c03619967f16830
Eric <ericyuanovo@gmail.com>
2026-01-14 11:34:07 +0800

    feat: 更新 template-react 至 vite8, 更新了package.json中的依赖

--- template-react/vite.config.ts
+++ template-react/vite.config.ts
@@ -1,36 +1,43 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig, loadEnv } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-    const isProd = mode === "production"
+    const isProd = mode === "prod"
     const env = loadEnv(mode, process.cwd()) // add the third param '', expected all variables in env files
 
     return {
         base: env.VITE_BUILD_BASE_PATH,
-        plugins: [tsconfigPaths({ loose: true }), react(), tailwindcss()],
-        build: {
-            sourcemap: !isProd,
-            manifest: true,
-            minify: "esbuild",
-            rollupOptions: {
-                // outDir: path.join(__dirname, 'dist', env.VITE_BUILD_OUT_PATH),
-                output: {
-                    manualChunks(id) {
-                        if (id.includes("node_modules")) {
-                            return id.toString().split("node_modules/")[1].split("/")[0].toString()
-                        }
-                    },
-                    chunkFileNames: "assets/js/[name]-[hash].js",
-                    entryFileNames: "assets/js/[name]-[hash].js",
-                    assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
-                }
-            }
+        resolve: {
+            // vite8新增
+            tsconfigPaths: true
         },
-        esbuild: {
-            target: "es2020"
+        plugins: [react(), tailwindcss()],
+        build: {
+            // outDir: env.VITE_BUILD_OUT_PATH,
+            // vite8 默认配置
+            // cssMinify: "lightningcss",
+            // minify:"oxc",
+            sourcemap: !isProd,
+            target: "esnext",
+            rolldownOptions: {
+                output: {
+                    entryFileNames: "assets/js/[name]-[hash].js",
+                    chunkFileNames: "assets/js/[name]-[hash].js",
+                    assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+                },
+                minify: isProd
+                    ? {
+                          terserOptions: {
+                              compress: {
+                                  drop_console: true,
+                                  drop_debugger: true
+                              }
+                          }
+                      }
+                    : false
+            }
         },
         server: {
             host: "0.0.0.0",
